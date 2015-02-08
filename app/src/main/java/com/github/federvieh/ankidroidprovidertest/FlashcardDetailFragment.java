5fe437acf643a90b22ff2ce8deda0d43bef1308c
Frank Oltmanns <frank.oltmanns+github@gmail.com>
2015-02-08 19:45:11 +0100

    Support changing main contents (e.g. tags).

--- app/src/main/java/com/github/federvieh/ankidroidprovidertest/FlashcardDetailFragment.java
+++ app/src/main/java/com/github/federvieh/ankidroidprovidertest/FlashcardDetailFragment.java
@@ -4,21 +4,34 @@
  */
 package com.github.federvieh.ankidroidprovidertest;
 
+import android.app.AlertDialog;
 import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A fragment representing a single Flash card detail screen.
@@ -39,6 +52,7 @@ public class FlashcardDetailFragment extends ListFragment implements LoaderManag
     private long noteId;
     private SimpleCursorAdapter mAdapter;
     private Cursor mCursor;
+    private TextView mHeaderView;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -55,11 +69,6 @@ public class FlashcardDetailFragment extends ListFragment implements LoaderManag
             noteId = getArguments().getLong(ARG_ITEM_ID);
         }
 
-        mAdapter = new SimpleCursorAdapter(getActivity(),
-                R.layout.detail_list_item, null,
-                new String[] {FlashCardsContract.DataColumns.MIMETYPE, FlashCardsContract.DataColumns.DATA1, FlashCardsContract.DataColumns.DATA2 },
-                new int[] { R.id.textMimetype, R.id.textData1, R.id.textData2 }, 0);
-        setListAdapter(mAdapter);
     }
 
     @Override
@@ -70,42 +79,170 @@ public class FlashcardDetailFragment extends ListFragment implements LoaderManag
         // or start a new one.
         getLoaderManager().initLoader(0, null, this);
 
-        getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                if(mCursor!=null) {
-                    mCursor.moveToPosition(position);
-                    long noteId = mCursor.getLong(1);
-                    String mimetype = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.MIMETYPE));
-                    String data1 = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.DATA1));
-                    String data2 = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.DATA2));
-                    Log.i("FlashCardListFragment", "Item long clicked, noteID: " + noteId);
-                    Log.i("FlashCardListFragment", "Item long clicked, MIME type: " + mimetype);
-                    Log.i("FlashCardListFragment", "Item long clicked, DATA1: " + data1);
-                    Log.i("FlashCardListFragment", "Item long clicked, DATA2: " + data2);
+        if(mHeaderView == null){
+            mHeaderView = new TextView(getActivity());
+            ContentResolver cr = getActivity().getContentResolver();
+            Cursor cursor = cr.query(Uri.withAppendedPath(FlashCardsContract.CONTENT_URI, Long.toString(noteId)), null, null, null, null);
+            StringBuilder noteContent = new StringBuilder();
+            if(cursor.moveToFirst()){
+                Log.i("FlashCardListFragment", "Cursor for: " + noteId);
+                String[] columnNames = cursor.getColumnNames();
+                for(int i = 0; i < cursor.getColumnCount(); i++) {
+                    Log.i("FlashCardListFragment", "Column: " + columnNames[i]);
+                    noteContent.append(columnNames[i]);
+                    noteContent.append(": ");
+                    noteContent.append(cursor.getString(i));
+                    noteContent.append("\n");
+                }
+                noteContent.deleteCharAt(noteContent.length()-1);
+            }
+            else {
+                noteContent.append("Whoopsie, no result!?");
+            }
+            mHeaderView.setText(noteContent);
+        }
+        getListView().addHeaderView(mHeaderView);
 
+        if(mAdapter==null){
+            mAdapter = new SimpleCursorAdapter(getActivity(),
+                    R.layout.detail_list_item, null,
+                    new String[] {FlashCardsContract.DataColumns.MIMETYPE, FlashCardsContract.DataColumns.DATA1, FlashCardsContract.DataColumns.DATA2 },
+                    new int[] { R.id.textMimetype, R.id.textData1, R.id.textData2 }, 0);
+        }
+
+        setListAdapter(mAdapter);
+    }
+
+    private void showEditDataDialog(int position){
+        if (mCursor != null) {
+            mCursor.moveToPosition(position - 1);//position 0 is mHeaderView
+            final long noteId = mCursor.getLong(1);
+            final String mimetype = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.MIMETYPE));
+            final String data1 = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.DATA1));
+            final String data2 = mCursor.getString(mCursor.getColumnIndex(FlashCardsContract.DataColumns.DATA2));
+            Log.i("FlashCardListFragment", "Item long clicked, noteID: " + noteId);
+            Log.i("FlashCardListFragment", "Item long clicked, MIME type: " + mimetype);
+            Log.i("FlashCardListFragment", "Item long clicked, DATA1: " + data1);
+            Log.i("FlashCardListFragment", "Item long clicked, DATA2: " + data2);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.change_card_data, null);
+            final EditText editTextMimeType = (EditText) dialogView.findViewById(R.id.editTextMimeType);
+            editTextMimeType.setText(mimetype);
+            final EditText editTextData1 = (EditText) dialogView.findViewById(R.id.editTextData1);
+            editTextData1.setText(data1);
+            final EditText editTextData2 = (EditText) dialogView.findViewById(R.id.editTextData2);
+            editTextData2.setText(data2);
+            builder.setView(dialogView);
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
                     ContentValues values = new ContentValues();
-                    values.put(FlashCardsContract.DataColumns.MIMETYPE, mimetype);
-                    values.put(FlashCardsContract.DataColumns.DATA1, data1);
-                    values.put(FlashCardsContract.DataColumns.DATA2, data2+"-");
+                    values.put(FlashCardsContract.DataColumns.MIMETYPE, editTextMimeType.getText().toString());
+                    values.put(FlashCardsContract.DataColumns.DATA1, editTextData1.getText().toString());
+                    values.put(FlashCardsContract.DataColumns.DATA2, editTextData2.getText().toString());
                     ContentResolver cr = getActivity().getContentResolver();
                     Uri noteUri = Uri.withAppendedPath(FlashCardsContract.CONTENT_URI, Long.toString(noteId));
                     Uri dataUri = Uri.withAppendedPath(noteUri, "data");
 
                     cr.update(dataUri, values, null, null);
                 }
-                else{
-                    Log.i("FlashCardListFragment", "Item clicked: " + position + ", but no cursor");
+            });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Do nothing, just close dialog
                 }
-                return true;
+            });
+            builder.create().show();
+        } else {
+            Log.i("FlashCardListFragment", "Item clicked: " + position + ", but no cursor");
+        }
+    }
+
+    private void showEditMainDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final ScrollView dialogView = (ScrollView) inflater.inflate(R.layout.change_card_main, null);
+        final LinearLayout list = (LinearLayout) dialogView.findViewById(R.id.blabla);
+
+//        final ListView listViewEditMain = (ListView) dialogView.findViewById(R.id.listViewMainEdit);
+
+//        final ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        final ContentResolver cr = getActivity().getContentResolver();
+        final Cursor cursor = cr.query(Uri.withAppendedPath(FlashCardsContract.CONTENT_URI, Long.toString(noteId)), null, null, null, null);
+        if(cursor.moveToFirst()){
+            Log.i("FlashCardListFragment", "Cursor for: " + noteId);
+            String[] columnNames = cursor.getColumnNames();
+            for(int i = 0; i < cursor.getColumnCount(); i++) {
+                View eme = inflater.inflate(R.layout.edit_main_entry, null);
+                ((EditText)eme.findViewById(R.id.editTextColumnContent)).setText(cursor.getString(i));
+                ((TextView)eme.findViewById(R.id.textViewColumnName)).setText(columnNames[i]);
+                list.addView(eme);
+                Log.i("FlashCardListFragment", "Column: " + columnNames[i]);
+//                list.add(putData(columnNames[i], cursor.getString(i)));
+            }
+        }
+
+        builder.setView(dialogView);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Check which items have changed and send update
+                ContentValues values = new ContentValues();
+                for(int i = 0; i < list.getChildCount(); i++){
+                    View v = list.getChildAt(i);
+                    String newText = ((EditText)v.findViewById(R.id.editTextColumnContent)).getText().toString();
+                    String origText = cursor.getString(i);
+                    String columnName = cursor.getColumnName(i);
+                    if(origText.equals(newText)){
+                        Log.i("FlashCardListFragment", columnName+" wasn't changed");
+                    }
+                    else{
+                        Log.i("FlashCardListFragment", columnName+" was changed from '"+ origText +"' to '" + newText + "'.");
+                        values.put(columnName, newText);
+                    }
+                }
+                if (values.size()>0){
+                    Uri noteUri = Uri.withAppendedPath(FlashCardsContract.CONTENT_URI, Long.toString(noteId));
+
+                    cr.update(noteUri, values, null, null);
+                }
             }
         });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Do nothing, just close dialog
+            }
+        });
+        builder.create().show();
+    }
 
+    private HashMap<String, String> putData(String columnName, String content) {
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("columnName", columnName);
+        item.put("content", content);
+        return item;
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        setListAdapter(null);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+        if (position>0) {
+            showEditDataDialog(position);
+        }
+        else if (position == 0) {
+            showEditMainDialog();
+        }
+        //else WTF!?
     }
 
     @Override
